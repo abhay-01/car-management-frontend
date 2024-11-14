96d5fd4cbdad9d241d96cb986f38153c09fffd88
abhay-01 <pratapsinghabhay999@gmail.com>
2024-11-14 22:35:30 +0530

    fix:routes

--- src/App.js
+++ src/App.js
@@ -1,41 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Dashboard from './pages/Dashboard';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import { getCars } from './services/carService';
-import CreateCar from './pages/CreateCar';
-import CarDetail from './pages/CarDetail';
-import EditCar from './pages/EditCar';
-
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Dashboard from "./pages/Dashboard";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import { getCars } from "./services/carService";
+import CreateCar from "./pages/CreateCar";
+import CarDetail from "./pages/CarDetail";
+import EditCar from "./pages/EditCar";
 
 const App = () => {
-
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [cars, setCars] = useState([]);
   const [isLogout, setIsLogout] = useState(false);
 
   useEffect(() => {
-    if (localStorage.getItem('token')) {
+    if (localStorage.getItem("token")) {
       setIsAuthenticated(true);
       fetchCars(); // Fetch cars when authenticated on initial load
     }
   }, []);
 
   const handleLogout = () => {
-    localStorage.removeItem('token');
+    localStorage.removeItem("token");
     setIsAuthenticated(false);
     setIsLogout(true);
   };
 
   useEffect(() => {
-
-    if(isLogout){
+    if (isLogout) {
       setIsLogout(false);
-      window.location.href = '/login';
+      window.location.href = "/login";
     }
-  }, [isLogout]);  
+  }, [isLogout]);
 
   const fetchCars = async () => {
     const carList = await getCars();
@@ -46,13 +43,47 @@ const App = () => {
     <Router>
       <Header isAuthenticated={isAuthenticated} onLogout={handleLogout} />
       <Routes>
-        {
-          isLogout?<Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} fetchCars={fetchCars} />} />:null
-        }
-        <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} fetchCars={fetchCars} />} />
+        {isLogout ? (
+          <Route
+            path="/login"
+            element={
+              <Login
+                setIsAuthenticated={setIsAuthenticated}
+                fetchCars={fetchCars}
+              />
+            }
+          />
+        ) : null}
+        <Route
+          path="/login"
+          element={
+            <Login
+              setIsAuthenticated={setIsAuthenticated}
+              fetchCars={fetchCars}
+            />
+          }
+        />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard cars={cars} fetchCars={fetchCars} />} />
-        <Route path="/" element={<Dashboard cars={cars} fetchCars={fetchCars} />} />
+        <Route
+          path="/dashboard"
+          element={<Dashboard cars={cars} fetchCars={fetchCars} />}
+        />
+        {isAuthenticated ? (
+          <Route
+            path="/dashboard"
+            element={<Dashboard cars={cars} fetchCars={fetchCars} />}
+          />
+        ) : (
+          <Route
+            path="/login"
+            element={
+              <Login
+                setIsAuthenticated={setIsAuthenticated}
+                fetchCars={fetchCars}
+              />
+            }
+          />
+        )}
         <Route path="/create" element={<CreateCar />} />
         <Route path="/car/:id" element={<CarDetail />} />
         <Route path="/car/:id/edit" element={<EditCar />} />
